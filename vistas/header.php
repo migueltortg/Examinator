0e9c08862403aef2845da140cd2ebc3ae02e71ff
migueltortg <migueltortg@gmail.com>
2023-11-04 11:22:19 +0100

    Intento solucionar problema con index.css

---
+++ vistas/header.php
@@ -0,0 +1,3 @@
+<div>
+    <img src="../css/img/icono.png" alt="" width="100px" height="110px">
+</div>
---
+++ vistas/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Examinator</title>
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+    <header>
+        <?php
+            require_once 'header.php';
+        ?>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
